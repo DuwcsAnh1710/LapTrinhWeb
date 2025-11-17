30318f24e79b1ca3c037de445a31ce617bd6a4f5
DuwcsAnh1710 <75246anhhao@gmail.com>
2025-11-17 20:56:22 +0700

    addb1

---
+++ Tuan09/module1/b1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Thông tin sinh viên</title>
+</head>
+<body>
+    <?php
+        $name = "Nguyễn Văn A";
+        $dob = "01/01/2005";
+        $class = "CTK45A";
+
+        echo "<h2>Thông tin sinh viên</h2>";
+        echo "Họ và tên: $name <br>";
+        echo "Ngày sinh: $dob <br>";
+        echo "Lớp: $class <br>";
+    ?>
+</body>
+</html>  
